1574ea355be6d590bfb00bb302e0d78eae4c8a8c
YL <simon-ling@hotmail.com>
2021-10-18 12:43:53 -0400

    done

--- Tutorial/Tutorial/AppMasterDetailPageMaster.xaml.cs
+++ Tutorial/Tutorial/AppMasterDetailPageMaster.xaml.cs
@@ -37,6 +37,7 @@ namespace Tutorial
                     new AppMasterDetailPageMenuItem { Id = 1, Title = "Entries",TargetType = typeof(Entries)},
                     new AppMasterDetailPageMenuItem { Id = 2, Title = "Buttons",TargetType = typeof(Buttons)},
                     new AppMasterDetailPageMenuItem { Id = 3, Title = "Images",TargetType = typeof(Images)},
+                    new AppMasterDetailPageMenuItem { Id = 3, Title = "EnrollStudentPage",TargetType = typeof(EnrollStudentPage)},
                     //new AppMasterDetailPageMenuItem { Id = 4, Title = "Page 5" },
                 });
             }
---
+++ Tutorial/Tutorial/EnrollStudentPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnrollStudentPage : ContentPage
+    {
+        public EnrollStudentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_OnClicked(object sender, EventArgs e)
+        {
+            lblDisplayMsg.Text = entName.Text
+                                 + ", we sent verification link to "
+                                 + entEmail.Text
+                                 + ". Please verify your account";
+
+        }
+    }
+}
